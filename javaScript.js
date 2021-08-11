7fe5d48cacf57499598c8e5aad470c77c10ac4d2
ckpapani <ckpapani@gmail.com>
2021-08-11 23:53:16 +0300

    .

--- javaScript.js
+++ javaScript.js
@@ -11,6 +11,8 @@ $( "#accordion2" ).accordion({
 
 
 
+
+
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function myFunction() {
   var x = document.getElementById("myLinks");
