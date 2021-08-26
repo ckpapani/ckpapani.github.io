abbb838bab2b4de1973d6600f4d0eb1fda512670
ckpapani <ckpapani@gmail.com>
2021-08-26 15:00:26 +0300

    thumbnails

--- javaScript.js
+++ javaScript.js
@@ -17,9 +17,34 @@ $( "#accordion3" ).accordion({
 });
 
 
+ 
+let v = document.getElementsByClassName("youtube-player"); 
+  
+for (let n = 0; n < v.length; n++) { 
+      v[n].onclick = function () { 
+        let iframe = document.createElement("iframe"); 
+        iframe.setAttribute("src", "https://www.youtube.com/embed/" + this.dataset.id); 
+        iframe.setAttribute("style", "width: 100%; height: 100%; position: absolute; top: 0; left: 0;");
+        iframe.setAttribute("allow","accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture");
+         
+      
+        if (this.dataset.fullscreen == 1){ 
+          iframe.setAttribute("allowfullscreen", "");
+        } 
+        while (this.firstChild) { 
+          this.removeChild(this.firstChild); 
+        } 
+        this.appendChild(iframe); 
+      }; 
+    } 
 
 
 
+
+    /* 
+    iframe.setAttribute("frameborder", "0"); 
+        iframe.setAttribute("id", "youtube-iframe");
+    + "?autoplay=0&autohide=2&border=0&wmode=opaque&enablejsapi=1&rel="+ this.dataset.related +"&controls="+this.dataset.control+"&showinfo=" + this.dataset.info */
 
 
 
